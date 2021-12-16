54629d46c3e30c021d756f60293eebf5ebd89181
Filip <szymanski.fp@gmail.com>
2021-12-16 21:19:22 +0100

    created JPanel with Button and TextField

--- src/gui/component/AddingComponent.java
+++ src/gui/component/AddingComponent.java
@@ -1,2 +1,19 @@
-package gui.component;public class AddingComponent {
+package gui.component;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class AddingComponent extends JPanel {
+    JButton button;
+    JTextField textField;
+
+
+    public AddingComponent(LayoutManager layout){
+        super(layout);
+        button = new JButton("Add");
+        textField = new JTextField();
+
+        add(button);
+        add(textField);
+    }
 }
