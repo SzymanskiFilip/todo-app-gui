f2c06c9f9052e2141719aa297720c3656ebf6ae1
Filip <szymanski.fp@gmail.com>
2021-12-16 21:18:56 +0100

    added AddingComponent JPanel to frame

--- src/gui/Frame.java
+++ src/gui/Frame.java
@@ -1,13 +1,20 @@
 package gui;
 
+import gui.component.AddingComponent;
+
 import javax.swing.*;
+import java.awt.*;
 
 public class Frame extends JFrame {
     JFrame frame;
-
+    AddingComponent addingComponent;
 
     public Frame(){
         frame = new JFrame("Todo App");
+        // Add components here
+        frame.getContentPane().add(new AddingComponent(new GridLayout(1,2)));
+
+
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(1000, 600);
         frame.setResizable(false);
