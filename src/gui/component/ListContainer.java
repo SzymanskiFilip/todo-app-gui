a4e2cddb39325c99618fb23885297ef3e84b7776
Filip <szymanski.fp@gmail.com>
2021-12-18 20:24:22 +0100

    created Class ListContainer that is a JPanel child and will hold list of todos

---
+++ src/gui/component/ListContainer.java
@@ -0,0 +1,10 @@
+package gui.component;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ListContainer extends JPanel {
+    public ListContainer(){
+        this.setBackground(Color.blue);
+    }
+}
