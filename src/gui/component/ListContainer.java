b53fcea30efbff8b1d10088428747c25e1168a4b
Filip <szymanski.fp@gmail.com>
2021-12-18 22:54:22 +0100

    added ListContainer that is a JPanel that works as a centerer-div

--- src/gui/component/ListContainer.java
+++ src/gui/component/ListContainer.java
@@ -5,6 +5,7 @@ import java.awt.*;
 
 public class ListContainer extends JPanel {
     public ListContainer(){
-        this.setBackground(Color.blue);
+       this.setLayout(new GridBagLayout());
+       this.add(new JButton("!!!!!"));
     }
 }
