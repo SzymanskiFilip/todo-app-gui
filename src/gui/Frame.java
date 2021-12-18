81d6a5bc50dc7d66fa87ae43944f365757ae250c
Filip <szymanski.fp@gmail.com>
2021-12-18 17:56:10 +0100

    added AdditionContainer to Frame

--- src/gui/Frame.java
+++ src/gui/Frame.java
@@ -1,9 +1,13 @@
 package gui;
 
+import gui.component.AdditionComponent;
+
 import javax.swing.*;
 import java.awt.*;
 
+
 public class Frame extends JFrame {
+
     public Frame(){
         initializeFrame();
     }
@@ -11,10 +15,17 @@ public class Frame extends JFrame {
 
     public void initializeFrame(){
         this.setTitle("Hello!");
-        this.getContentPane().setBackground(Color.GRAY);
+        this.getContentPane().setBackground(Color.darkGray);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(1000, 600);
         this.setResizable(false);
+        this.setLayout(new BorderLayout());
+        addComponents();
         this.setVisible(true);
+    }
+
+    public void addComponents(){
+        //add components
+        this.getContentPane().add(new AdditionComponent(), BorderLayout.NORTH);
     }
 }
