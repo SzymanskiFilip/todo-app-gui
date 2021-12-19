b95f6358a73ceb21601c932ac0f64545415063e7
Filip <szymanski.fp@gmail.com>
2021-12-19 22:20:21 +0100

    dummy data list

--- src/main/java/gui/component/ListComponent.java
+++ src/main/java/gui/component/ListComponent.java
@@ -5,11 +5,12 @@ import java.awt.*;
 
 public class ListComponent extends JList {
 
-    private static String[] options = {"1","2","3","4","5","6","7"};
+    private static String[] options = {"12222222222222222","2","3","4","5","6","7"};
 
     public ListComponent(){
         super(options);
         setLayout(new FlowLayout());
-        
+        setVisibleRowCount(20);
+        setPreferredSize(new Dimension(400, 200));
     }
 }
