e005ecb2b1f959edd70e2179ae50b7ee18a4519f
Filip <szymanski.fp@gmail.com>
2021-12-19 21:04:18 +0100

    created list component that will hold the todos

--- src/main/java/gui/component/ListComponent.java
+++ src/main/java/gui/component/ListComponent.java
@@ -1,9 +1,15 @@
 package gui.component;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class ListComponent extends JList {
+
+    private static String[] options = {"1","2","3","4","5","6","7"};
+
     public ListComponent(){
+        super(options);
+        setLayout(new FlowLayout());
         
     }
 }
