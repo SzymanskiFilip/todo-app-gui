b4802142ef8ca2b68937285601b561a95f66a442
Filip <szymanski.fp@gmail.com>
2021-12-19 22:20:50 +0100

    list container is a box layout that is a row of components

--- src/main/java/gui/component/ListContainer.java
+++ src/main/java/gui/component/ListContainer.java
@@ -4,9 +4,14 @@ import javax.swing.*;
 import java.awt.*;
 
 public class ListContainer extends JPanel {
+
+
     public ListContainer(){
-       this.setLayout(new GridBagLayout());
+       this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
        this.setBackground(Color.magenta);
+
+       this.add(new JButton("hello"));
+       this.add(new JButton("hellooooo"));
        this.add(new ListComponent());
     }
 }
