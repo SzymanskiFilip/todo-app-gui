4394c371b45de9cac8a0ebe41e1807bec21d5baa
Filip <szymanski.fp@gmail.com>
2021-12-22 19:24:35 +0100

    added Group root to Scene, and set scene of the stage

--- src/main/java/eu/filip/todoappgui/Main.java
+++ src/main/java/eu/filip/todoappgui/Main.java
@@ -1,11 +1,23 @@
 package eu.filip.todoappgui;
 
 import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
     @Override
     public void start(Stage stage) throws Exception {
 
+        Group root = new Group();
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.show();
     }
 }
