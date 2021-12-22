0f440d9e255b54686456043e0f99c6eaf460fc15
Filip <szymanski.fp@gmail.com>
2021-12-22 19:32:49 +0100

    created 1280x720 window

--- src/main/java/eu/filip/todoappgui/Main.java
+++ src/main/java/eu/filip/todoappgui/Main.java
@@ -3,6 +3,8 @@ package eu.filip.todoappgui;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -15,9 +17,14 @@ public class Main extends Application {
     public void start(Stage stage) throws Exception {
 
         Group root = new Group();
-        Scene scene = new Scene(root);
+        Scene scene = new Scene(root, Color.BLACK);
 
+        stage.setWidth(1280);
+        stage.setHeight(720);
+        stage.setResizable(false);
+        stage.setTitle("Todo App");
         stage.setScene(scene);
+
         stage.show();
     }
 }
