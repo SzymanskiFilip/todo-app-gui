57c9718199e2e2b7d21626845a74b59a6713e93d
Filip <szymanski.fp@gmail.com>
2021-12-25 00:13:07 +0100

    fixed deleting from list

--- src/main/java/eu/filip/todoappgui/Controller.java
+++ src/main/java/eu/filip/todoappgui/Controller.java
@@ -1,17 +1,20 @@
 package eu.filip.todoappgui;
 
+import javafx.beans.InvalidationListener;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.ResourceBundle;
+import java.util.*;
 
 public class Controller implements Initializable {
 
@@ -19,15 +22,27 @@ public class Controller implements Initializable {
     private ListView<String> list;
     @FXML
     private TextField textField;
+    @FXML
+    private Button deleteButton;
 
     ArrayList<String> todoList = new ArrayList<>();
     String currentTodo;
 
     public void add(ActionEvent e){
         try{
-            System.out.println(textField.getText());
             todoList.add(textField.getText());
             list.getItems().add(textField.getText());
+            textField.setText("");
+        } catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+
+    public void delete(ActionEvent e){
+        try{
+            System.out.println("DELETING: " + currentTodo);
+            list.getItems().remove(list.getItems().indexOf(currentTodo));
+            todoList.remove(todoList.indexOf(currentTodo));
         } catch (Exception ex){
             ex.printStackTrace();
         }
