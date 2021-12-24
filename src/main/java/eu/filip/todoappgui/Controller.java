08e49d9cc8bb6e0e207d3d534735b585e0a608f7
Filip <szymanski.fp@gmail.com>
2021-12-24 22:11:50 +0100

    added controller class

---
+++ src/main/java/eu/filip/todoappgui/Controller.java
@@ -0,0 +1,9 @@
+package eu.filip.todoappgui;
+
+import javafx.event.ActionEvent;
+
+public class Controller {
+    public void add(ActionEvent e){
+        System.out.println(e.getSource());
+    }
+}
