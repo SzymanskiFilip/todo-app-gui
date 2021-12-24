36ee1264126f119b3dd10f523a0d3f0b9c37ee7b
Filip <szymanski.fp@gmail.com>
2021-12-25 00:29:04 +0100

    created FileReader class that has two static methods overrideTodos and getTodos

--- src/main/java/eu/filip/todoappgui/FileReader.java
+++ src/main/java/eu/filip/todoappgui/FileReader.java
@@ -1,2 +1,36 @@
-package eu.filip.todoappgui;public class FileReader {
+package eu.filip.todoappgui;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class FileReader {
+    public static ArrayList<String> getTodos(){
+        File file = new File("src/todos.txt");
+        ArrayList<String> todos = new ArrayList<>();
+        try{
+            Scanner scanner = new Scanner(file);
+            while(scanner.hasNextLine()){
+                todos.add(scanner.nextLine());
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return todos;
+    }
+
+    public static void overrideTodos(ArrayList<String> todosToOverride){
+        File file = new File("src/todos.txt");
+        try{
+            FileWriter fileWriter = new FileWriter(file);
+            String toWrite = "";
+            for(String todo : todosToOverride){
+                toWrite += todo + "\n";
+            }
+            fileWriter.write(toWrite);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 }
