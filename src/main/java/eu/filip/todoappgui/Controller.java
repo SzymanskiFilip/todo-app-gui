6f34df8d447ff935ae96a35ae7b60e00007dadd3
Filip <szymanski.fp@gmail.com>
2021-12-24 22:16:58 +0100

    added @FXML annotation to inject fxml into the Controller class, and set the textField to the textfield in scenebuilder

--- src/main/java/eu/filip/todoappgui/Controller.java
+++ src/main/java/eu/filip/todoappgui/Controller.java
@@ -1,9 +1,23 @@
 package eu.filip.todoappgui;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 
 public class Controller {
+
+    @FXML
+    private ListView<String> list;
+    @FXML
+    private TextField textField;
+
     public void add(ActionEvent e){
-        System.out.println(e.getSource());
+        try{
+            System.out.println(textField.getText());
+        } catch (Exception ex){
+            ex.printStackTrace();
+        }
+
     }
 }
